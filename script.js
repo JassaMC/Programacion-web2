38e1f63b6f744a055947ce3f421409341322723e
JassaMC <jmendezc2201@alumno.ipn.mx>
2024-09-24 18:10:30 -0600

    Creacion de directorio y archivo

---
+++ script.js
@@ -0,0 +1,29 @@
+
+//CLASE 24/09/2024
+//NOMBRE:Cruz Enriquez Bruno y Mendez Castro Jali Sarai
+
+//VARIABLES
+/*
+LAS VARIABLES GUARDAN INFORMACION QUE PUEDES REUTILIZAR 
+*/
+
+//Declaracion de variables 
+
+let nombre = 'Pedro';
+// usa let para declarar variables que pueden cambiar
+const PI = 3.1416;
+var edad = 25;
+
+//operadores
+let a = 5;
+let b = 2;
+let suma = a + b;//7
+let resta = a - b;//3
+let multiplicacion  = a * b;//10
+let division = a/b;//2.5
+
+//operadores de comparacion
+a > b;//true
+a < b;//false
+a == b;//false
+a === b;//false (compara valor)
